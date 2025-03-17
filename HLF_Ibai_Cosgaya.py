b16686dd2330687ed39f834a222122b83290b174
IbaiCosgaya <koskiman19@gmail.com>
2025-03-17 12:29:25 +0100

    finish_hundir_la_flota

--- HLF_Ibai_Cosgaya.py
+++ HLF_Ibai_Cosgaya.py
@@ -4,23 +4,7 @@ init(autoreset=True)
 import string
 import random
 import os
-
-def iniciar_juego():
-    #preguntas iniciales para inicializar el juego
-    nombre_usuario = input("Dime tu nombre, humano: ").capitalize()
-
-    pregunta_empezar = input(f"Hola, {nombre_usuario}, ¿Quieres jugar a 'Hundir la Flota'? ")
-    while pregunta_empezar.lower() not in ["si", "no"]:
-        print("Por favor, responde con 'si' o 'no'")
-        pregunta_empezar = input("Quieres jugar a 'Hundir la Flota'? ")
-
-    if pregunta_empezar.lower() == "no":
-        print("Cobarde, ¡Hasta luego!")
-        return None
-
-    elif pregunta_empezar.lower() == "si":
-        print(f"Genial, que empiece el juego, mucha suerte, {nombre_usuario}")
-        return nombre_usuario
+import time
 
 class Tablero:
 
@@ -32,8 +16,8 @@ class Tablero:
             fila =  ["."] * 10
             #append de los puntos al tablero
             self.tablero.append(fila)
-            
-        self.barcos = []
+
+        # self.barcos = []
 
     def convertir_letra_numero(self,letra):
         # metodo para convertir la letra de la columna a numero para la colocacion y disparo
@@ -43,8 +27,7 @@ class Tablero:
         #devolvemos el indice de la letra a buscar
         return letras.index(letra)
 
-    def colocar_barcos_usuario(self):
-        print("Coloca tus barcos")
+    def colocar_barcos_usuario_manual(self):
         barcos_usuario = {1: 4, 2: 3, 3: 2, 4: 1}  # GENERAMOS DICCIONARIO DE BARCOS CON ESLORA:CANTIDAD
 
         for eslora, cantidad in barcos_usuario.items():
@@ -65,40 +48,76 @@ class Tablero:
                             continue
 
                         # Colocar el barco en el tablero
-                        self.colocacion_usuario(fila, columna, eslora, orientacion)
+                        self.colocacion_usuario_manual(fila, columna, eslora, orientacion)
+                        self.mostrar_tablero_usuario()
                         break
                     except Exception as e:
                         print(f"Error: {e}. Intenta de nuevo.")
 
-    def colocar_barcos_pc(self):
-        barcos_pc = {1: 4, 2: 3, 3: 2, 4: 1}
-
-        for eslora, cantidad in barcos_pc.items():
+    def colocar_barcos_usuario_aleatorio(self):
+        # {4: 1, 3: 2, 2: 3, 1: 4}
+        barcos_usuario = {4: 1, 3: 2, 2: 3, 1: 4}  # Barcos con eslora:cantidad
+        for eslora, cantidad in barcos_usuario.items():
             for _ in range(cantidad):  # Coloca la cantidad necesaria de cada tipo
-                while True:
+                # intentos = 0
+                # max_intentos = 1000  # Límite de intentos para colocar un barco
+                while True:#intentos < max_intentos:
                     try:
-                        fila = random.randint(1,9)
-                        random_num = random.randint(1,9)
-                        letras = string.ascii_uppercase[0:len(self.tablero)]
-                        columna = letras[random_num]
-                        columna = self.convertir_letra_numero(columna)  # CONVERTIMOS A INT LA LETRA CON EL METODO DE CONVERTIR LETRA
-                        orientacion = "H"
-                        if eslora > 1:
-                            opciones_letra = ["H", "V"]
-                            random_opciones = random.randint(0,1)
-                            orientacion = opciones_letra[random_opciones]
+                        fila = random.randint(0, 9)
+                        columna = random.randint(0, 9)
+                        orientacion = random.choice(["H", "V"])
 
                         # Validar la posición del barco
                         if not self.validar_posicion(fila, columna, eslora, orientacion):
+                            # intentos += 1
+                            continue
+
+                        # Colocar el barco en el tablero
+                        self.colocacion_usuario_aleatorio(fila, columna, eslora, orientacion)
+                        break
+                    except Exception as e:
+                        #intentos += 1  # Incrementar intentos en caso de error
+                        continue
+
+                # if intentos == max_intentos:  # Si no se puede colocar el barco
+                #     print(f"Error: No se pudo colocar un barco de eslora {eslora}.")
+                #     return
+        time.sleep(2)
+        # self.mostrar_tablero_usuario()
+
+    def colocar_barcos_pc(self):
+        barcos_pc = {4: 1, 3: 2, 2: 3, 1: 4}
+
+        for eslora, cantidad in barcos_pc.items():
+            for _ in range(cantidad): # Coloca la cantidad necesaria de cada tipo
+                # intentos = 0
+                # max_intentos = 1000
+                while True:#intentos < max_intentos:
+                    try:
+                        fila = random.randint(0, 9)
+                        columna = random.randint(0, 9)
+                        orientacion = random.choice(["H", "V"])
+
+                        # Validar la posición del barco
+                        if not self.validar_posicion(fila, columna, eslora, orientacion):
+                            # intentos += 1
                             continue
 
                         # Colocar el barco en el tablero
                         self.colocacion_pc(fila, columna, eslora, orientacion)
                         break
                     except Exception as e:
-                        pass
+                        # intentos += 1  # Incrementar intentos en caso de error
+                        continue
 
-    def colocacion_usuario(self, fila, columna, eslora, orientacion):
+                # if intentos == 100:  # Si no se puede colocar el barco
+                #     print(f"Error: No se pudo colocar un barco de eslora {eslora}.")
+                #     return
+
+        time.sleep(2)
+        # self.mostrar_tablero_pc()
+
+    def colocacion_usuario_manual(self, fila, columna, eslora, orientacion):
         if orientacion == "H":
             for i in range(eslora):
                 self.tablero[fila][columna + i] = Fore.RED + str("B") + Style.RESET_ALL
@@ -106,12 +125,20 @@ class Tablero:
             for i in range(eslora):
                 self.tablero[fila + i][columna] = Fore.RED + str("B") + Style.RESET_ALL
         print("Barco colocado correctamente.")
-        self.mostrar_tablero()
+        self.mostrar_tablero_usuario()
+
+    def colocacion_usuario_aleatorio(self, fila, columna, eslora, orientacion):
+        if orientacion == "H":
+            for i in range(eslora):
+                self.tablero[fila][columna + i] = Fore.RED + str("B") + Style.RESET_ALL
+        elif orientacion == "V":
+            for i in range(eslora):
+                self.tablero[fila + i][columna] = Fore.RED + str("B") + Style.RESET_ALL
 
     def colocacion_pc(self, fila, columna, eslora, orientacion):
         if orientacion == "H":
             for i in range(eslora):
-                self.tablero[fila][columna + i] = Fore.GREEN + str("B") + Style.RESET_ALL
+                self.tablero[fila][columna + i] =  Fore.GREEN + str("B") + Style.RESET_ALL
         elif orientacion == "V":
             for i in range(eslora):
                 self.tablero[fila + i][columna] = Fore.GREEN + str("B") + Style.RESET_ALL
@@ -124,7 +151,7 @@ class Tablero:
                 return False
             # Comprobar que no haya otros barcos
             for i in range(eslora):
-                if self.tablero[fila][columna + i] != "." or self.barcos_alrededor(fila, columna+1):
+                if self.tablero[fila][columna + i] != "." or self.barcos_alrededor(fila, columna+i):
                     return False
         elif orientacion == "V":
             # Comprobar si cabe verticalmente
@@ -132,7 +159,7 @@ class Tablero:
                 return False
             # Comprobar que no haya otros barcos
             for i in range(eslora):
-                if self.tablero[fila + i][columna] != "." or self.barcos_alrededor(fila+1, columna):
+                if self.tablero[fila + i][columna] != "." or self.barcos_alrededor(fila+i, columna):
                     return False
         return True
 
@@ -140,35 +167,61 @@ class Tablero:
 
         for x in range(fila-1, fila+2):
             for i in range(columna-1, columna+2):
-                if 0<=x<10 and 0<=i<10:
-                    if self.tablero[x][i] == "B":
+                if 0 <= x <10 and 0 <= i <10:
+                    if "B" in self.tablero[x][i]:
                         return True
         return False
 
-    def disparar_usuario(self,fila,columna):
-        print("Tu turno")
-        fila = int(input("Introduce la fila (0-9): "))
-        columna = input("Introduce la letra (A-J): ").upper()
-        columna = self.convertir_letra_numero(columna)
-
-        tocado = False
-        if self.tablero[fila][columna] == "B":
-            print("Tocado")
-            tocado = True
-        elif self.tablero[fila][columna] == ".":
-            print("Agua")
-            tocado = False
-        else:
-            print("Introduce las coordenadas correctas")
-
+    def disparar_usuario(self, tablero_pc):
         while True:
-            self.tablero[fila][columna] = "X"
+            try:
+                print("Dispara")
+                fila = int(input("Introduce la fila (0-9): "))
+                columna = input("Introduce la letra (A-J): ").upper()
+                columna = self.convertir_letra_numero(columna)
 
-        if not tocado:
-            self.tablero[fila][columna] = "o"
+                if not (0<=fila<10) or not (0<=columna<10):
+                    print("Fuera del tablero")
+                    continue
+                if tablero_pc.tablero[fila][columna] in ["X", "o"]:
+                    print("Espabila, ya has disparado ahí")
+                    continue
 
-    def disparar_pc(self):
-        pass
+                if "B" in tablero_pc.tablero[fila][columna]:
+                    print("¡Tocado! Sigue disparando.")
+                    tablero_pc.tablero[fila][columna] = Fore.YELLOW + str("X") + Style.RESET_ALL
+                    tablero_pc.mostrar_tablero_pc()
+                    return True
+
+                else:
+                    miss_choices = ["Pringao", "Fallaste", "Tolay", "Melon", "Agua"]
+                    print(random.choice(miss_choices))
+                    tablero_pc.tablero[fila][columna] = Fore.CYAN + str("o") + Style.RESET_ALL
+                return False
+
+            except Exception as e:
+                print(f"Error: {e}. Intenta de nuevo.")
+
+    def disparar_pc(self, tablero_usuario):
+        while True:
+            fila = random.randint(0,9)
+            columna = random.randint(0,9)
+            # columna = self.convertir_letra_numero(columna)
+
+            if tablero_usuario.tablero[fila][columna] in ["X", "o"]:
+                print("Fallé")
+                continue
+
+            if "B" in tablero_usuario.tablero[fila][columna]:
+                print("¡Acerté! Vuelvo a disparar.")
+                tablero_usuario.tablero[fila][columna] = Fore.YELLOW + str("X") + Style.RESET_ALL
+                tablero_usuario.mostrar_tablero_usuario()
+                return True
+
+            else:
+                tablero_usuario.tablero[fila][columna] = Fore.CYAN + str("o") + Style.RESET_ALL
+
+            return False
 
     def mostrar_tablero(self):
         headers = []
@@ -184,25 +237,94 @@ class Tablero:
 
         print(Style.BRIGHT+tabulate(columna_numeros,headers=[""]+ headers, stralign="center", tablefmt="rounded_grid"))  # 'HEADERS' ES EL HEADERS DE LA TABLA
 
+    def mostrar_tablero_usuario(self):
+        # os.system("cls")
+        self.mostrar_tablero()
 
-# iniciar_juego()
-tablero_usuario = Tablero()
-tablero_pc = Tablero()
+    def mostrar_tablero_pc(self):
+        # os.system("cls")
+        self.mostrar_tablero()
 
-tablero_usuario.mostrar_tablero()
-tablero_usuario.colocar_barcos_usuario()
-# os.system("cls")
-# tablero_usuario.mostrar_tablero()
+    def mostrar_tablero_pc_oculto(self):
+        headers = []
+        for x in string.ascii_uppercase[0:len(self.tablero)]:  # IMPORTAMOS EL ALFABETO Y HACEMOS QUE VAYA DE LA 'A' A LA 'J'
+            headers.append(x)
+
+        columna_numeros = []
+        for x in range(len(self.tablero)):  # CHECKEAMOS LA LONGITUD DE LOS ELEMENTOS DE TABLERO
+            fila_oculta = [str(x)]  # METEMOS CADA ELEMENTO EN UNA LISTA TIPO STRING EN VEZ DE INT
+            for elemento in self.tablero[x]:  # RECORREMOS LOS ELEMENTOS LA FILA ORIGINAL
+                if elemento == Fore.YELLOW + "X" + Style.RESET_ALL:  # Acierto
+                    fila_oculta.append(elemento)
+                elif elemento == Fore.CYAN + "o" + Style.RESET_ALL:  # Agua
+                    fila_oculta.append(elemento)
+                else:  # Celdas no reveladas
+                    fila_oculta.append(" ")  # Espacio vacío para ocultar barcos
+            columna_numeros.append(fila_oculta)  #
+
+        print(Style.BRIGHT + tabulate(columna_numeros, headers=[""] + headers, stralign="center",tablefmt="rounded_grid"))
 
 
-# tablero_pc.colocar_barcos_pc()
-# tablero_pc.mostrar_tablero()
+def iniciar_juego():
+    #preguntas iniciales para inicializar el juego
+    nombre_usuario = input("Dime tu nombre, humano: ").capitalize()
+    pregunta_empezar = input(f"Hola, {nombre_usuario}, ¿Quieres jugar a 'Hundir la Flota'? ")
 
-# tablero_pc.barcos_alrededor(0,0)
+    while pregunta_empezar.lower() not in ["si", "no"]:
+        pregunta_empezar = input("Por favor, responde con 'si' o 'no'")
+
+    if pregunta_empezar.lower() == "no":
+        print("Cobarde, ¡Hasta luego!")
+        return
+
+    elif pregunta_empezar.lower() == "si":
+        print(f"Genial, que empiece el juego, mucha suerte, {nombre_usuario}")
+
+    tablero_usuario = Tablero()
+    tablero_pc = Tablero()
+
+    print("Estoy colocando mis barcos...\n")
+    tablero_pc.colocar_barcos_pc()
+
+    aleatorio = int(input(f"{nombre_usuario} Pulsa '1' para colocacion manual o '2' para colocacion aleatoria\n"))
+    if aleatorio == 1:
+        tablero_usuario.colocar_barcos_usuario_manual()
+    elif aleatorio == 2:
+        print("Colocando tus barcos...\n")
+        tablero_usuario.colocar_barcos_usuario_aleatorio()
+
+    turno_usuario = True
 
 
+    while True:
+        print(f"Tu tablero, {nombre_usuario}: \n")
+        tablero_usuario.mostrar_tablero_usuario()
+
+        print("Mi tablero:")
+        tablero_pc.mostrar_tablero_pc()
+        tablero_pc.mostrar_tablero_pc_oculto()
+
+        if turno_usuario:
+            print(f"Tu turno, {nombre_usuario}")
+            repite_turno = tablero_usuario.disparar_usuario(tablero_pc)
+
+            if not any("B" in "".join(fila) for fila in tablero_pc.tablero):
+                print("¡Felicidades! Has hundido todos mis barcos")
+                break
+
+            if not repite_turno:
+                turno_usuario = False  # Cambiar turno al PC
+
+        else:
+            print("Me toca")
+            repite_turno = tablero_pc.disparar_pc(tablero_usuario)
+
+            if not any("B" in "".join(fila) for fila in tablero_usuario.tablero):  # Verificar si quedan tus barcos
+                print("He ganado, he hundido todos tus barcos")
+                break
+
+            if not repite_turno:
+                turno_usuario = True  # Cambiar turno al usuario
 
 
-
-
-
+iniciar_juego()
--- README.md
+++ README.md
@@ -1,4 +1,64 @@
 # HLF_Ibai_Cosgaya
 ejercicio de hundir la flota
 
-La idea principal es crear una clase Tablero que mediante sus metodos se puedan crear tanto los tableros del user y del pc como sus funciones.
+La idea principal es crear una clase Tablero que mediante sus metodos se puedan crear tanto los tableros del user y del pc como sus funciones de disparar. Una vez creada la clase Tablero, creo una funcion que ayuda a inicializar el juego, con preguntas del nombre del usuario y de si se quiere jugar. Despues se crean dos objetos: el pc y el usuario. Despues de esto, con un bucle ingreso la logica del juego con los metodos y se valida si uno de los dos jugadores ha ganado.
+
+He tenido que importar algunas librerias para poder llevar a cabo el proyecto. La que más juego me ha dado ha sido "Tabulate" para generar tablas y "Colorama" para crear colores para los barcos y ayudar a la visualizacion. Luego la libreria "randon" me ha ayudado a generar las coordenadas aleatorias de la colocacion del pc como del user.
+
+Logica dentro de Tablero:
+
+                             GENERACION DE LOS TABLEROS
+
+ 1. Con el atributo principal self.tablero se genera la tabla de 10*10 utilizando una lista de listas
+
+ 2. Le doy una forma mas visual a ese tablero con mostrar_tablero añadiendole headers y letras en las columnas. Esas casillas no forman parte de las coordenadas de la matriz.
+
+ 3. Le asigno el tablero al user y al pc (sin ocultar) con mostrar_tablero_pc y mostrar_tablero_usuario. El tablero del usuario muestra sus barcos y disparos realizados por el PC. El tablero del PC incluye inicialmente solo los barcos del ordenador.
+
+ 5. Con alguna modificacion genero el tablero oculto del pc. Se ocultan las posiciones de los barcos. Solo se muestran los disparos del usuario (aciertos y fallos).
+
+                            COLOCACION DE LOS BARCOS Y VALIDACION
+
+ 4. Mediante colocar_barcos_pc se colocan barcos en el tablero del PC en posiciones aleatorias:
+    Se seleccionan coordenadas iniciales aleatorias.
+    Se elige una orientación (horizontal o vertical).
+    Se valida que la posición sea válida (validar_posicion).
+    Si la posición es válida, se colocan las celdas del barco en el tablero.
+
+5. Colocación Manual de Barcos para el Usuario (colocar_barcos_usuario_manual). Permite al jugador decidir manualmente las posiciones de sus barcos:
+    El usuario proporciona coordenadas iniciales y orientación.
+    Se valida la posición.
+    Si es válida, se actualizan las celdas del tablero.
+
+6. Con (validar_posicion), se verifica que:
+
+    El barco no exceda los límites del tablero.
+    No haya barcos en las casillas seleccionadas ni en las adyacentes (barcos_alrededor).
+
+
+                            DISPAROS
+
+7. Disparos del Usuario (disparar_usuario) permite al jugador atacar el tablero del PC:
+
+    El usuario introduce coordenadas.
+    Se verifica que la casilla no haya sido disparada antes.
+    Si el disparo impacta un barco, la casilla se marca como "X". Si falla, se marca como "o".
+
+8. Misma logica para los disparos del pc
+
+                            TURNOS Y LOGICA DEL JUEGO
+Turnos Alternos
+
+    En el turno del jugador, se llama a disparar_usuario.
+    En el turno del PC, se llama a disparar_pc.
+    Si un jugador acierta un disparo, puede repetir su turno.
+
+Determinación del Ganador.
+En cada turno, se verifica si alguno de los jugadores ha perdido todos sus barcos:
+
+    Se utiliza una comprobación en el tablero (any("B" in "".join(fila) for fila in tablero)).
+    Si todos los barcos han sido destruidos, el juego termina.
+
+
+
+
